4a02051d64ab4940cfdf1855b15c3306bfbf52c9
m-pasima <theivykierachronicles@gmail.com>
2025-08-12 14:24:07 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,61 @@
-variable "project_name"    { type = string, default = "teaching-eks" }
-variable "region"          { type = string, default = "eu-west-2" }
-variable "cluster_version" { type = string, default = "1.29" }
+variable "project_name" {
+  type        = string
+  default     = "teaching-eks"
+  description = "Name prefix for resources"
+}
+
+variable "region" {
+  type        = string
+  default     = "eu-west-2"
+  description = "AWS region"
+}
+
+variable "cluster_version" {
+  type        = string
+  default     = "1.29"
+  description = "EKS control plane version"
+}
 
 # Spot node group sizing (group A)
-variable "spot_instance_types" { type = list(string), default = ["t3.small"] }
-variable "spot_min_size"       { type = number, default = 1 }
-variable "spot_desired_size"   { type = number, default = 1 }
-variable "spot_max_size"       { type = number, default = 1 }
+variable "spot_instance_types" {
+  type        = list(string)
+  default     = ["t3.small"]
+  description = "Instance types for the SPOT group"
+}
+
+variable "spot_min_size" {
+  type        = number
+  default     = 1
+}
+
+variable "spot_desired_size" {
+  type        = number
+  default     = 1
+}
+
+variable "spot_max_size" {
+  type        = number
+  default     = 1
+}
 
 # On-demand node group sizing (group B)
-variable "od_instance_types"   { type = list(string), default = ["t3.small"] }
-variable "od_min_size"         { type = number, default = 1 }
-variable "od_desired_size"     { type = number, default = 1 }
-variable "od_max_size"         { type = number, default = 1 }
+variable "od_instance_types" {
+  type        = list(string)
+  default     = ["t3.small"]
+  description = "Instance types for the ON_DEMAND group"
+}
+
+variable "od_min_size" {
+  type        = number
+  default     = 1
+}
+
+variable "od_desired_size" {
+  type        = number
+  default     = 1
+}
+
+variable "od_max_size" {
+  type        = number
+  default     = 1
+}
