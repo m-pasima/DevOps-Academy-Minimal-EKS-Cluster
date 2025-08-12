6d92a20b042eb55966e71b2888b8f8d5210fdc6b
m-pasima <theivykierachronicles@gmail.com>
2025-08-12 14:23:01 +0100

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,7 +1,12 @@
 terraform {
   required_version = ">= 1.6.0, < 2.0.0"
+
   backend "s3" {}
+
   required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 5.60" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.60"
+    }
   }
 }
