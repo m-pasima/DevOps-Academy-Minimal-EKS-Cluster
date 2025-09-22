1e4d8bc44dbf0b873380140df3a99ddbdea61e70
m-pasima <theivykierachronicles@gmail.com>
2025-09-22 14:34:42 +0100

    added apply file

--- bootstrap-iam/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region = var.region
-}
-
-data "aws_partition" "current" {}
--- bootstrap-iam/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "region"     { type = string, default = "eu-west-2" }
-variable "role_name"  { type = string, default = "github-oidc-eks-admin" }
-variable "github_org" { type = string } # e.g. "PassyOrg"
-variable "repo_name"  { type = string } # e.g. "eks-teaching"
-variable "branch"     { type = string, default = "main" }
